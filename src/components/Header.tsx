5c9c280c70cdc1c48f2add1cd1e67d69d891618a
Marcus H <marcus@gatekeeper.network>
2023-05-21 19:31:07 -0600

    adding logo and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import { Navbar, Button, Link, Text, Spacer } from "@nextui-org/react";
+import { styled } from "@nextui-org/react"
+import logo from "../logo0.png";
+
+export const Box = styled("div", {
+  boxSizing: "border-box",
+});
+export const Content = () => (
+    <Box css={{px: "$12", mt: "$8", "@xsMax": {px: "$10"}}}>
+      <Text h2>Lorem ipsum dolor sit amet</Text>
+      <Text size="$lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Purus gravida quis blandit turpis. Augue neque gravida in
+        fermentum et sollicitudin ac orci. Et sollicitudin ac orci phasellus egestas. Elementum tempus
+        egestas sed sed risus pretium quam vulputate. Interdum velit euismod in pellentesque massa
+        placerat duis ultricies.
+      </Text>
+      <Spacer y={1} />
+      </Box>
+)
+
+
+// export const Layout = ({ child}) => (
+//     <Box
+//       css={{
+//         maxW: "100%"
+//       }}
+//     >
+//       {child}
+//       <Content />
+//     </Box>
+//   );
+
+export function Header() {
+    return (
+        <Box
+        css={{
+          maxW: "100%"
+        }}
+      >
+        <Navbar isBordered variant="sticky">
+          <Navbar.Brand
+          css={{
+            maxW: "50%",
+            minH: "100%",
+            align: "left"
+          }}>
+            {/* <AcmeLogo /> */}
+             <img src={logo} />
+            {/* <Text b color="inherit" hideIn="xs">
+              Semaphore Network
+            </Text> */}
+          </Navbar.Brand>
+          <Navbar.Content hideIn="xs" variant="underline">
+            <Navbar.Link isActive href="#">Wallet</Navbar.Link>
+            <Navbar.Link href="#">Network</Navbar.Link>
+            <Navbar.Link href="#">Home</Navbar.Link>
+          </Navbar.Content>
+          <Navbar.Content>
+            <Navbar.Link color="inherit" href="#">
+              Become A Provider
+            </Navbar.Link>
+            <Navbar.Item>
+              <Button auto flat as={Link} href="#">
+                Use The Network
+              </Button>
+            </Navbar.Item>
+          </Navbar.Content>
+        </Navbar>
+      </Box>
+    )
+  }
